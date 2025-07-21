f00f0b2e728b6738a1c4e0a314b56cb2848d8e8e
Mohammad Hosein Kazemainy <m.h.kazemainy@gmail.com>
2025-07-21 13:11:22 +0330

    add 04

---
+++ codes/04-for-list-tuple-dict-set.py
@@ -0,0 +1,67 @@
+#for in range str
+#list
+#append extend insert remove pop clear index count sort reverse copy
+#tuple
+#count index
+#dict
+#clear copy fromkeys get items keys values pop popitem setdefault update
+#set
+#add update remove discard pop clear copy union intersection difference symmetric_difference
+#isdisjoint issubset issuperset intersection_update difference_update symmetric_difference_update
+#len
+
+#توضیحات تکمیلی و مثال های بیشتر از متدها و عملکرد هر کدام از ساختار داده ها داخل کلاس داده شد
+
+
+#حلقه روی اعداد و رشته
+# i="test"
+# print(i)
+
+# for i in range(15,2,-2):
+#     print(i)
+
+# s="salam gcc :)"
+# for i in s:
+#     print(i.upper())
+
+# print(i)
+
+
+#مثال از لیست
+# l=list()
+# # l=[]
+# while True:
+#     s=input("add item (exit):")
+#     if s=="exit":break
+#     l.append(s)
+# print(l)
+
+
+#مثال از دیکشنری
+# d={}
+# while True:
+#     l=input("add key value (exit):").split()
+#     if l[0]=="exit":break
+#     d[l[0]]=l[1]
+# print(d)
+
+# d={'ali': '25', 'hasan': '30', 'hosein': '35'}
+
+# for k in d.keys():
+#     print(k)
+# for v in d.values():
+#     print(v)
+# for k,v in d.items():
+#     print(k,v)
+#     # d[k]=int(v)
+# print(d)
+
+
+#مثال از جابجایی دو متغیر
+# a=10
+# b=20
+# a,b=b,a
+# # temp=a
+# # a=b
+# # b=temp
+# print(a,b)
